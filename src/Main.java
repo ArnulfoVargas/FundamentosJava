cf9bda10f3b7824e0d06689592c8f57d44c30290
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-06-11 09:59:37 -0600

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        var num = 10;
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+        boolean continueAsking = false;
+        float val = 0;
 
-        num += 10;
+        do{
+            System.out.println("Type a value to add\nCurrent value: " + val);
+            val = Sum(val, console.nextFloat());
 
-        System.out.println("num = " + num);
+            System.out.println("Add more? (Type true or false)");
+            continueAsking = console.nextBoolean();
+        }while (continueAsking);
 
-        var txt = "hola";
-        txt += " mundo";
+        System.out.println("Result = " + val);
+    }
 
-        System.out.println("txt = " + txt);
-
-
+    private static float Sum(float a, float b)
+    {
+        return a + b;
     }
 }
